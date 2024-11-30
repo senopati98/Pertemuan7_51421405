94cc9fc7ba0a26d19e5aab18308a4cf7eb0f83ab
senopati98 <153344590+senopati98@users.noreply.github.com>
2024-11-30 22:36:11 +0700

    Add files via upload

---
+++ M1/Persegipanjang.java
@@ -0,0 +1,15 @@
+public class Persegipanjang {
+    public static void main(String[] args) {
+        // Panjang dan lebar ditentukan secara langsung
+        int panjang = 10;
+        int lebar = 5;
+
+        // Menghitung luas dan keliling
+        int luas = panjang * lebar;
+        int keliling = 2 * (panjang + lebar);
+
+        // Output hasil
+        System.out.println("Luas: " + luas);
+        System.out.println("Keliling: " + keliling);
+    }
+}
---
+++ M1/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    public static void main(String[] args) {
+        String name;
+        int age;
+
+        name = "Senopati Khalifah Zalatin S";
+        age = 21;
+
+        String education = "Mahasiswa";
+
+        System.out.println("Nama saya adalah " + name);
+        System.out.println("Umur saya " + age + " tahun");
+        System.out.print("Saya adalah Seorang " + education);
+
+    }
+}
